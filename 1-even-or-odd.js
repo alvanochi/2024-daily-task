345017d83ce4b5ca60b954e83ccda16766615834
= <=>
2024-02-01 23:44:33 +0700

    day 1/366

---
+++ 1-even-or-odd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+evenOrOdd(2);
+evenOrOdd(7);
+evenOrOdd(-42);
+evenOrOdd(-7);
+evenOrOdd(0);
+
+//daily-task
+//1 - 366
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/solutions/javascript
