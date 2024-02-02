dfad31c346fa77d5fba294eb348f0976f867dfd5
= <=>
2024-02-02 22:53:16 +0700

    day 3/366

---
+++ 3-makeNegative.js
@@ -0,0 +1,10 @@
+const makeNegative = (num) => (num == 0 ? num : -Math.abs(num));
+
+console.log(makeNegative(1));
+console.log(makeNegative(0));
+console.log(makeNegative(-5));
+console.log(makeNegative(0.12));
+
+//daily-task
+//3 - 366
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/solutions/javascript
