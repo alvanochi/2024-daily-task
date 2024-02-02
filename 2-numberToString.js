e653ae69ef65332ef3be45864f4cb644cd8c1fce
= <=>
2024-02-02 11:17:10 +0700

    day 2/366

---
+++ 2-numberToString.js
@@ -0,0 +1,20 @@
+// function numberToString(num) {
+//   // Return a string of the number here!
+//   let result = "";
+//   return (result += num);
+// }
+
+//const numberToString = (num) => {
+//     return `${num}`
+//   };
+
+const numberToString = (num) => {
+  let result = "";
+  return (result += num);
+};
+
+console.log(numberToString(67));
+
+//daily-task
+//day 2 - 366
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8/solutions/javascript
