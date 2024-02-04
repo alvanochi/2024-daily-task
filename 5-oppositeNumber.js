a0d11a6aba17a00de6bcbf514bdff54330550ed3
= <=>
2024-02-04 23:24:30 +0700

    day 5/366

---
+++ 5-oppositeNumber.js
@@ -0,0 +1,10 @@
+const opposite = (number) => (number ? -number : number);
+
+console.log(opposite(7));
+console.log(opposite(-5));
+console.log(opposite(0));
+console.log(opposite(3.333333));
+
+//daily-task
+//day 5 - 366
+//https://www.codewars.com/kata/56dec885c54a926dcd001095
