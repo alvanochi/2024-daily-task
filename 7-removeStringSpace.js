1e6d475d9ab3adce61cdf172c8b3e692d3814d80
= <=>
2024-02-06 01:32:46 +0700

    day 7/366

---
+++ 7-removeStringSpace.js
@@ -0,0 +1,9 @@
+const noSpace = (x) => x.split(" ").join("");
+
+console.log(noSpace("8aaaaa dddd r     "));
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+console.log(noSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+
+//daily-task
+//day 7-366
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5
