063a6ecff9971d81373d6931ad20335e39aec605
= <=>
2024-02-05 22:17:42 +0700

    day 6/366

---
+++ 6-reversedStrings.js
@@ -0,0 +1,14 @@
+// const solution = (str) => {
+//   const arrKosong = [];
+//   for (let i = 0; i < str.length; i++) {
+//     arrKosong.push(str[i]);
+//   }
+//   return arrKosong.reverse().join("");
+// };
+// console.log(solution("hello"));
+
+const solution = (str) => str.split("").reverse().join("");
+
+//daily-task
+//3 - 366
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018
