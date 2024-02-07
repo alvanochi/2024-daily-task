2f350fc0d576b001b800c6d01bb34f01eb876225
= <=>
2024-02-07 23:27:32 +0700

    day 9/366

---
+++ 9-stringRepeat.js
@@ -0,0 +1,9 @@
+const repeatStr = (n, s) => s.repeat(n);
+
+console.log(repeatStr(3, "*"));
+console.log(repeatStr(5, "#"));
+console.log(repeatStr(2, "ha "));
+
+//daily-task
+//day 9-366
+//https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/
