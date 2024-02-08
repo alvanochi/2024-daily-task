d78044809bd457eb8d527ab98d788cb338274e0c
= <=>
2024-02-08 15:50:36 +0700

    day 10/366

---
+++ 10-stringToNumber.js
@@ -0,0 +1,9 @@
+const stringToNumber = (str) => Number(str);
+
+console.log(stringToNumber("1234"));
+console.log(stringToNumber("0"));
+console.log(stringToNumber("-1"));
+
+//daily-task
+//day 10-366
+//https://www.codewars.com/kata/544675c6f971f7399a000e79/
