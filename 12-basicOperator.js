29961c46fe00fc232b1894a22074cd1aa39dc2f0
= <=>
2024-02-11 23:40:07 +0700

    day 12/366

---
+++ 12-basicOperator.js
@@ -0,0 +1,8 @@
+const basicOp = (operation, value1, value2) =>
+  eval(`${value1} ${operation} ${value2}`);
+
+console.log(basicOp("+", 4, 7));
+
+//daily-task
+//day 12 - 366
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7
