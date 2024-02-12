4e89dc0c3909b65888e41902d5e32cf7bad8b573
= <=>
2024-02-12 22:48:31 +0700

    day 14/366

---
+++ 14-returningStrings.js
@@ -0,0 +1,11 @@
+//Make a function that will return a greeting statement that uses an input;
+// your program should return,
+//"Hello, <name> how are you doing today?".
+
+const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+console.log(greet("Alvan"));
+
+//daily-task
+//day 14 - 366
+//https://www.codewars.com/kata/55a70521798b14d4750000a4
