e6b281f3c76e1a3ce8ce8f499700dd889486b3fa
= <=>
2024-02-17 00:01:51 +0700

    day 18/366

---
+++ 18-mapping.js
@@ -0,0 +1,10 @@
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+const maps = (x) => x.map((x) => x * 2);
+
+console.log(maps([1, 2, 3]));
+
+//daily-task
+//day 18 - 366
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e
