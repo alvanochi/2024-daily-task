a38488cb5695a2a5624bd32fb8a8944e631533b7
= <=>
2024-02-18 23:41:54 +0700

    day 20/366

---
+++ 20-booleanToString.js
@@ -0,0 +1,10 @@
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
+const booleanToString = (b) => `${b}`;
+
+console.log(booleanToString(true));
+
+//daily-task
+//day 20 - 366
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd
