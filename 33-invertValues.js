2b85c51f4a63370bc4450c64f2b0414d1ac54b93
= <=>
2024-03-03 08:56:57 +0700

    day 33/366

---
+++ 33-invertValues.js
@@ -0,0 +1,16 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+const invert = (array) => array.map((e) => (e ? -e : e));
+
+console.log(invert([1, -2, 3, -4, 5]));
+console.log(invert([0]));
+console.log(invert([]));
+
+//daily-task
+//day 33 - 366
+//https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
