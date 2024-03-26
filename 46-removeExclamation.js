62e1215da6384122398b081f97fc54ce23d5e994
= <=>
2024-03-26 22:37:26 +0700

    day 46/366

---
+++ 46-removeExclamation.js
@@ -0,0 +1,10 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+const removeExclamationMarks = (s) => s.replaceAll("!", "");
+
+console.log(removeExclamationMarks("Hello World!!!"));
+console.log(removeExclamationMarks(" awsdadaw!awdasw"));
+
+//daily-task
+//day 46 - 366
+//https://www.codewars.com/kata/57a0885cbb9944e24c00008e
